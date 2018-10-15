7487f3202288525bc45a5d66e0bf0d1a3eb609ca
Andiakatz4 <andiakatz@gmail.com>
2018-10-15 02:41:09 -0700

    added styling and php, part 2

---
+++ app/andia_wordpress_script.php
@@ -0,0 +1,15 @@
+//Part 2 of AlgaeCal Front-End Test
+
+<?php wp_enqueue_script("jquery"); ?>
+
+<?php wp_head(); 
+
+function shapeSpace_include_custom_jquery() {
+
+	wp_deregister_script('jquery');
+	wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js', array(), null, true);
+
+}
+add_action('wp_enqueue_scripts', 'shapeSpace_include_custom_jquery');
+
+?>
